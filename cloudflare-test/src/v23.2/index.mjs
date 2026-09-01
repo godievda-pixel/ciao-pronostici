db99e347d289f68487d9babc1cdf1e9c753856ce
godievda-pixel <godievda@gmail.com>
2026-09-01 18:45:12 +0300

    feat: expose inert v23.2 browser core

---
+++ cloudflare-test/src/v23.2/index.mjs
@@ -0,0 +1,32 @@
+import {
+  COMPETITION_KEYS,
+  COMPETITIONS,
+  getCompetitionConfig,
+} from './competition-config.mjs';
+import {
+  normalizeMatch,
+  normalizeTeam,
+  shouldIncludeMatch,
+} from './match-normalizer.mjs';
+import {
+  sortChronologically,
+  matchesForCompetition,
+  groupForCompetition,
+  availablePredictions,
+  nextMatchForTeam,
+} from './tournament-engine.mjs';
+
+globalThis.CiaoV232Core = Object.freeze({
+  version: '23.2-core',
+  competitions: COMPETITION_KEYS,
+  competitionConfig: COMPETITIONS,
+  getCompetitionConfig,
+  normalizeMatch,
+  normalizeTeam,
+  shouldIncludeMatch,
+  sortChronologically,
+  matchesForCompetition,
+  groupForCompetition,
+  availablePredictions,
+  nextMatchForTeam,
+});
